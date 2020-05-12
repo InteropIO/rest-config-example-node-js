f09ef1845af420070218fbab5780d86ab7524f7d
Georgi Davidkov <georgi.davidkov@tick42.com>
2020-05-12 10:17:51 +0300

    add support for environment and region
    add support for serving system configuration like - system,logger, channels,themes and etc

---
+++ src/configs.ts
@@ -0,0 +1,32 @@
+import { Express } from "express";
+import util from "util";
+import fs from "fs";
+import path from "path";
+import json5 from "json5";
+import { getUser, getRegionEnvFolder } from "./utils";
+
+const readFile = util.promisify(fs.readFile);
+
+export default function(app: Express, rootFolder: string) {
+    app.get('/configs', (req, res, next) => {
+        const user = getUser(req);
+        const config = req.header("config");
+        const filePath = path.join(getRegionEnvFolder(req, rootFolder), `${config}.json`);
+        fetchConfigurations(user, filePath)
+            .then(configs => {
+                res.setHeader('Content-Type', 'application/json');
+                res.end(JSON.stringify(configs));
+            }).catch(error => {
+                res.status(500);
+                console.log(error);
+                res.send(JSON.stringify(error));
+            })
+    });
+
+
+    async function fetchConfigurations(user: string, filePath: string) {
+        console.log(`fetching ${filePath} configuration for ${user}...`);
+        return await readFile(path.join(filePath), 'utf8').then(json5.parse);
+    }
+}
+
--- src/utils.ts
+++ src/utils.ts
@@ -3,3 +3,6 @@ import { Request as ExpressRequest } from "express";
 export function getUser(req: ExpressRequest): string {
     return req.header("impersonated_user") || req.header("user") || "<unknown>";
 }
+export function getRegionEnvFolder(req: ExpressRequest, filePath: string): string {
+    return filePath.replace("%ENV%", req.header("env") as string).replace("%REGION%", req.header("region") as string);
+}
